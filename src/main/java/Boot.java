46fe08ede7fa1e2812130285323bfda1270b4a21
dtan64 <dawa6951@colorado.edu>
2020-02-29 19:33:43 -0500

    init

---
+++ src/main/java/Boot.java
@@ -0,0 +1,15 @@
+/**
+ * @author D'Artagnan Wake
+ */
+public class Boot {
+
+
+	// -------------------- Main --------------------
+
+	public static final void main(String[] args) throws Exception {
+
+		for (int i = 0; i < 100; i++) {
+			System.err.printf("%s\n", "hit");
+		}
+	}
+}
